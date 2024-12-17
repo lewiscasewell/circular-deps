cb2aba68aafe13b8bdcc3b67b5d8b274134422b3
Lewis Casewell <lewiscasewell@hotmail.co.uk>
2024-12-17 09:38:15 +0000

    add in a circ dep

---
+++ packages/api/src/a.ts
@@ -0,0 +1,4 @@
+import { b } from './b';
+import { something } from './c';
+export const a = 'Hello from A';
+console.log(b, something);
---
+++ packages/api/src/b.ts
@@ -0,0 +1,4 @@
+import { a } from './a';
+
+export const b = 'Hello from B';
+console.log(a);
